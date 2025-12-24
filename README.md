6d1646b67b11871719973f401d8de6ec3e823422
SaFal Gautam <saphal106@gmail.com>
2025-12-24 19:13:26 +0545

    Done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Complaint Management API
+
+This is a basic Complaint Management API made by using tools such as `Express.js`, `MongoDB`, `Mongoose`.
+
+This follow basic *CRUD* concept to manage data
--- app.js
+++ app.js
@@ -7,8 +7,6 @@ app.use(express.json())
 
 const {create, fetchComplaints, fetchComplaint, update, deleteComplaint} = require("./controller/complaints")
 
-
-
 app.post("/create", create)
 app.get("/complaints", fetchComplaints)
 app.get("/complaint/:id", fetchComplaint)
@@ -19,4 +17,3 @@ app.delete("/delete/:id", deleteComplaint)
 app.listen(3000, function(){
     console.log("server started on port no 3000")
 })
-
